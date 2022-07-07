963563bc051f42677210259dab97a1e5f94b0c80
antoine.amend@gmail.com <antoine.amend@gmail.com>
2022-07-07 04:04:56 +0000

    link to databricks

--- 00_geofraud_context.py
+++ 00_geofraud_context.py
@@ -2,9 +2,9 @@
 # MAGIC %md
 # MAGIC <img src=https://d1r5llqwmkrl74.cloudfront.net/notebooks/fs-lakehouse-logo.png width="600px">
 # MAGIC 
-# MAGIC [![DBR](https://img.shields.io/badge/DBR-9.1ML-red?logo=databricks&style=for-the-badge)](.)
-# MAGIC [![CLOUD](https://img.shields.io/badge/CLOUD-ALL-blue?logo=googlecloud&style=for-the-badge)]()
-# MAGIC [![POC](https://img.shields.io/badge/POC-5_days-green?style=for-the-badge)]()
+# MAGIC [![DBR](https://img.shields.io/badge/DBR-9.1ML-red?logo=databricks&style=for-the-badge)](https://docs.databricks.com/release-notes/runtime/9.1ml.html)
+# MAGIC [![CLOUD](https://img.shields.io/badge/CLOUD-ALL-blue?logo=googlecloud&style=for-the-badge)](https://databricks.com/try-databricks)
+# MAGIC [![POC](https://img.shields.io/badge/POC-5_days-green?style=for-the-badge)](https://databricks.com/try-databricks)
 # MAGIC 
 # MAGIC *A large scale fraud prevention system is usually a complex ecosystem made of various controls (all with critical SLAs), a mix of traditional rules and AI and a patchwork of technologies between proprietary on-premises systems and open source cloud technologies. In a previous [solution accelerator](https://databricks.com/blog/2021/01/19/combining-rules-based-and-ai-models-to-combat-financial-fraud.html), we addressed the problem of blending rules with AI in a common orchestration layer powered by MLFlow. In this series of notebooks centered around geospatial analytics, we demonstrate how Lakehouse enables organizations to better understand customers behaviours, no longer based on who they are, but how they bank, no longer using a one-size-fits-all rule but a truly personalized AI. After all, identifying abnormal patterns can only be made possible if one first understands what a normal behaviour is, and doing so for millions of customers becomes a challenge that requires both data and AI combined into one platform. As part of this solution, we are releasing a new open source geospatial library, [GEOSCAN](https://github.com/databrickslabs/geoscan), to detect geospatial behaviours at massive scale, track customers patterns over time and detect anomalous card transactions*
 # MAGIC 
